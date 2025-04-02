dc99dd860074ccdbc4c6f35124b6e5dafa104b49
f-murillo <murillo.franc@gmail.com>
2025-04-02 11:49:56 -0400

    Update main.c

--- Proyecto 2/teopHilos/main.c
+++ Proyecto 2/teopHilos/main.c
@@ -2,13 +2,12 @@
 #include <stdlib.h>
 #include "teatro.h"
 
-// Funcion principal
 int main(int argc, char *argv[]){
-    if(argc != 3){ // En caso de ingresar mas o menos de 3 argumentos
+    if(argc != 3){ 
         printf("Error: numero de argumentos invalido. Ejemplo: %s [n] [archivo_instancia]\n", argv[0]);
         return 1;
     }
-    // Numero de hilos ingresado por el usuario, y el archivo con la instancia
+    // Numero de hilos, y archivo con la instancia
     int n = atoi(argv[1]); 
     char *file = argv[2];
     // Se ejecuta el teatro de operaciones
